795261453aa6cbc9025942a153fb3e6dc338ae18
OmarHumamah <omar.nabeel.h@gmail.com>
2021-09-07 18:20:18 +0300

    lab08

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require("dotenv").config;
 let express = require("express");
 let cors = require("cors");
 let weatherData = require("./data/weather.json");
+const { default: axios } = require("axios");
 
 let serverWeatherAPI = express();
 serverWeatherAPI.use(cors());
@@ -23,9 +24,10 @@ serverWeatherAPI.get("/weather", (req, res) => {
   let forCastResult = [];
 
   result.data.forEach((n, i) => {
-    forCastResult.push({ 
-        date : result.data[i].datetime,
-        description: `the high temp is ${result.data[i].high_temp} and the low temp is ${result.data[i].low_temp}, ${result.data[i].weather.description}`});
+    forCastResult.push({
+      date: result.data[i].datetime,
+      description: `the high temp is ${result.data[i].high_temp} and the low temp is ${result.data[i].low_temp}, ${result.data[i].weather.description}`,
+    });
   });
   console.log(lat);
   console.log(lon);
@@ -35,6 +37,49 @@ serverWeatherAPI.get("/weather", (req, res) => {
   res.send(forCastResult);
 });
 //...........................
+serverWeatherAPI.get("/weatherbit", (req, res) => {
+  let lat = req.query.lat;
+  let lon = req.query.lon;
+  let query = req.query.city;
+  let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.KEY || '32173c091a4645d08c10967920a7d2aa'}&city=${query}&lat=${lat}&lon=${lon}`;
+
+  axios.get(url).then((result) => {
+    let newForecast = [];
+    result.data.data.forEach((day) => {
+      newForecast.push(new Forecast(day));
+    });
+    res.send(newForecast);
+  });
+});
+
+function Forecast(day) {
+  (this.description = `Low of ${day.low_temp}, high of ${day.high_temp} with ${day.weather.description}`),
+    (this.date = day.datetime);
+}
+//............................
+serverWeatherAPI.get("/movies", (req, res) => {
+  let search = req.query.search;
+  let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.KEYMOVIE ||"d392d3ce0f9ceefcfd09b99d97173b24"}&query=${search}`;
+
+  axios.get(url).then((result) => {
+    let newMovies = [];
+    result.data.results.forEach((info) => {
+      newMovies.push(new Movie(info));
+    });
+    res.send(newMovies);
+  });
+});
+
+function Movie(info) {
+  this.title = info.original_title,
+    this.overview = info.overview,
+    this.average_votes= info.vote_average,
+    this.total_votes= info.vote_count,
+    this.image_url= `https://image.tmdb.org/t/p/w500${info.poster_path}`,
+    this.popularity= info.popularity,
+    this.released_on= info.release_date
+}
+//............................
 serverWeatherAPI.get("*", (req, res) => {
   res.status(500).send("the page is not found 500");
 });
