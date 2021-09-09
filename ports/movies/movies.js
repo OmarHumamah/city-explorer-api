e193d708f0509627e849de7710b494ffb52b330e
OmarHumamah <omar.nabeel.h@gmail.com>
2021-09-09 12:06:08 +0300

    lab10

--- ports/movies/movies.js
+++ ports/movies/movies.js
@@ -1,19 +1,29 @@
-const { default: axios } = require("axios");
 require("dotenv").config();
+const { default: axios } = require("axios");
+
+let myMemory = {};
+
 
 function gitMovies (req, res){
-    let search = req.query.search;
-    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.KEYMOVIE}&query=${search}`;
-  
-    axios.get(url).then((result) => {
-      let newMovies = [];
-      result.data.results.forEach((info) => {
-        newMovies.push(new movieCons(info));
-      });
-      res.send(newMovies);
-    });
-  }
-  
-  let movieCons = require('./movies_constructor')
+  let search = req.query.search;
+  if (myMemory[search] !== undefined ) {
+    res.send(myMemory[search])
+   }
+   else{
+
+     let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.KEYMOVIE}&query=${search}`;
+     axios.get(url).then((result) => {
+       let newMovies = [];
+       result.data.results.forEach((info) => {
+         newMovies.push(new movieCons(info));
+       });
+       myMemory[search] = newMovies
+       res.send(newMovies);
+     });
+   }
+   
+   let movieCons = require('./movies_constructor')
+   }
+   
 
   module.exports = gitMovies
